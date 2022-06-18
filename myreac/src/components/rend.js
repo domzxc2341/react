5bc773d59b3e63e636e3d482f9349076671c85b8
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-06-18 09:07:15 -0800

    ye
    ye

---
+++ myreac/src/components/rend.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const rend = () => {
+  return (
+    <div>
+        <p>potang ina gumana kana </p>
+
+    </div>
+  )
+}
---
+++ myreac/src/components/test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Test() {
+  return (
+    <div>
+        <p>potang ina gumana kana </p>
+
+    </div>
+  );
+}
+
