5c9b15a003bfd5d01d1869e68c1fe018d564aad8
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-06-25 15:34:03 -0800

    ye
    ye

--- myreac/src/App.js
+++ myreac/src/App.js
@@ -1,30 +1,17 @@
 
 import './sty.css'
-
-import React,{ useState } from 'react';
-
+import { Navbar } from './components/Navbar';
+/*import React,{ useState } from 'react';
+*/
 
 
 
 
 function App() {
-  const [count, setCount] = useState (0)
-
-  function Decrease(){
-    setCount(count - 1)
-   }
-   function Increase(){
-    setCount(count + 1)
-   }
-  return (
-   
-  <div>
-    <button onClick={Decrease}>-</button>
-    <h1> {count} </h1>
-    <button onClick={Increase}> +</button>
-  </div>
   
-    
+  return (
+   <Navbar />
+
   );
 
 }
