e63cd6da5b3754cd32161bed60f8502390712a8f
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-06-29 13:49:16 -0800

    ye
    ye

--- myreac/src/App.js
+++ myreac/src/App.js
@@ -1,25 +1,16 @@
 
 
 import Navbar from './components/Navbar';
-import logo1 from './deliver.jpg';
+
+
 import React, { Fragment } from 'react';
-import './components/image1.css';
-/*import React,{ useState } from 'react';
-*/
-
-
-
 
 function App() {
   
   return (
     <React.Fragment>
     <Navbar />
-<div>
-  <h1 className='text'>Next Time Nalang</h1>
-<img className='image' src={logo1} /> 
-<h1 className='text1 typewriter'>BIGLANG DELIVER BY RON LIKO</h1>
-</div>
+
 </React.Fragment>
   );
 
--- myreac/src/components/Navbar.js
+++ myreac/src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import './Navbars.css';
 
  function Navbar() {
   return (
+    <React.Fragment>
     <div className='navbar'>
       
-    <a className='navtext' href="./home"> Home </a>
+    <h1><a className='a1' href="./home"> Home </a></h1>
 
 
     </div>
+    <h1 className='text'><a>TEXT</a></h1>
+    </React.Fragment>
   )
 }
 
