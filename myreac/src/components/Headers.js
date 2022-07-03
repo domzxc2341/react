423321bc0dc5d7a1db0a76b194baff6ed33c9de4
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-07-02 21:28:43 -0800

    ye
    ye

--- myreac/src/components/Headers.js
+++ myreac/src/components/Headers.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import './Header.css'
+import Pota from './gray.jpg';
+
 
 const Headers = () => (
-    
+    <React.Fragment>
+        
     <div className='Head'>
-       <div className='Img'>
-
-
-       </div>
-       
+        <img src={Pota} alt='OO' className='Img' /> 
+        <h1 className='type1'>Hello,  I'm  Dominique  Simbillo</h1>
         </div>
-    
+        
+  
+  
+    </React.Fragment>
 )
 
 export default Headers
