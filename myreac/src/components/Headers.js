ece4b374a09bca428d49e9ef7dd96d1e1c4edcd8
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-07-03 19:24:27 -0800

    ye
    ye

--- myreac/src/components/Headers.js
+++ myreac/src/components/Headers.js
@@ -9,6 +9,8 @@ const Headers = () => (
     <div className='Head'>
         <img src={Pota} alt='OO' className='Img' /> 
         <h1 className='type1'>Hello,  I'm  Dominique  Simbillo</h1>
+        <h1 className='type2'>And I'm </h1>
+        <h1 className='pl1'>Web Developer</h1>
         </div>
         
   
