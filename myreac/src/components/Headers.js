33146702ea531b6844cdc4ded4be7e9bf8ebe230
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-07-06 11:10:54 -0800

    ye
    ye

--- myreac/src/components/Headers.js
+++ myreac/src/components/Headers.js
@@ -1,6 +1,6 @@
 import React,{Fragment} from 'react'
 import './Header.css'
-import Pota from './gray.jpg';
+import Pota from './profilebg.jpg';
 
 
 const Headers = () => (
@@ -9,8 +9,7 @@ const Headers = () => (
     <div className='Head'>
         <img src={Pota} alt='OO' className='Img' /> 
         <h1 className='type1'>Hello,  I'm  Dominique  Simbillo</h1>
-        <h1 className='type2'>And I'm </h1>
-        <h1 className='pl1'>Web Developer</h1>
+        <h1 className='type2'>And I'm Web Developer </h1>
         </div>
         
   
